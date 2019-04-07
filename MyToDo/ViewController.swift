34d04915ac126d78aca2ab03bc3b43882492fb22
Ramesh <hanumanthappar32@gmail.com>
2019-04-07 15:23:57 +0530

    todo list complete with data source and delegate methods

--- MyToDo/ViewController.swift
+++ MyToDo/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    
+    let persons = ["Ramesh", "Jalaja", "shilpa", "Adarsh", "Sheetal"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    //Mark - Table view data source method
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return persons.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+     let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for:indexPath)
+        cell.textLabel?.text = persons[indexPath.row]
+        return cell
+    }
+    
+    
+//Mark - Tableview Delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print (persons[indexPath.row])
+        
+        //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType  == .checkmark  {
+           tableView.cellForRow(at: indexPath)?.accessoryType  = .none
+            
+        } else {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
